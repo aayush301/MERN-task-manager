08cb3d42b789ad44626d62324bec291ad8b084a8
Aayush <aayush5521186@gmail.com>
2022-05-12 15:43:07 +0530

    prepared for deployment to heroku

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const path = require("path");
 const cors = require("cors");
 require("dotenv").config();
 const authRoutes = require("./routes/authRoutes");
@@ -18,10 +19,17 @@ mongoose.connect(mongoUrl, err => {
 });
 
 
-app.get("/", (req, res) => res.json({ msg: "Hello from express app" }));
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/profile", profileRoutes);
+
+
+// For deployment to heroku
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "../frontend/build")));
+  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend/build/index.html")));
+}
+
 
 
 const port = process.env.PORT || 5000;
